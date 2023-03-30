1f3bdf5e4c4a698d53a485599122fa78d4319a98
alaordedalus <115832364+alaordedalus@users.noreply.github.com>
2023-03-29 21:03:20 -0300

    alaor123

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import mba from './Creysson.jpg';
+import mba from './123.jpg';
 import './App.css';
 
 class App extends Component {
